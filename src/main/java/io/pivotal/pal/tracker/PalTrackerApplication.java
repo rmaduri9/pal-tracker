c6cc918d63594c1d66d51424a9660adce07ae2eb
Rohit Maduri <rmaduri1@ford.com>
2021-04-20 19:23:46 +0000

    ci pipeline

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,4 +1,18 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
 public class PalTrackerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PalTrackerApplication.class, args);
+        // new SpringApplicationBuilder(PalTrackerApplication.class)
+        //   .web(WebApplicationType.NONE)
+        //    .run(args);
+    }
 }
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,20 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    private final String welcome_message;
+
+    public WelcomeController(@Value("${welcome.message}") String welcome_message) {
+        this.welcome_message = welcome_message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return this.welcome_message;
+    }
 }
+
